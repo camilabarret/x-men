09db7108f99db8917e089c28ece5f8b7ed3b29c0
Camila Barreto <138591662+camilabarret@users.noreply.github.com>
2023-07-12 16:50:21 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,34 @@
+// OBJ 1:passo 1 - pegar os personagens no JS pra poder verificar quando o usuário passar o mouse em cima de um deles.
+const personagens = document.querySelectorAll('.personagem');
+
+// passo 2 - adicionar a classe selecionado no personagem que o usuário passar o cursor do mouse.
+personagens.forEach((personagem) => {
+	personagem.addEventListener('mouseenter', () => {
+		
+		// uma gambiara para visualizar melhor no celular.
+		if(window.innerWidth < 450) {
+			window.scrollTo({top: 0, behavior: 'smooth'});
+		}
+		
+		// passo 3 - verificar se já existe um personagem selecionado, se sim, devemos remover a seleção dele.
+		const personagemSelecionado = document.querySelector('.selecionado');
+		personagemSelecionado.classList.remove('selecionado')
+		
+		personagem.classList.add('selecionado');
+		
+		// OBJ 2: passo 1 - pegar o elemento do personagem grande pra adicionar as informações nele.
+		const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+		
+		// passo 2 - alterar a imagem do personagem grande.
+		const idPersonagem = personagem.attributes.id.value;
+		imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+		
+		// passo 3 - alterar o nome do personagem grande.
+		const nomePersonagem = document.getElementById('nome-personagem');
+		nomePersonagem.innerText = personagem.getAttribute('data-name');
+		
+		// passo 4 - alterar a descrição do personagem grande
+		const descricaoPersonagem = document.getElementById('descricao-personagem');
+		descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+	})
+})
